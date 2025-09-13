35b6b3b508b435e0fe8658ecc92b175c3cfee8a3
Rafael Adriano <144788933+jabalxp@users.noreply.github.com>
2025-09-13 01:37:48 -0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import { Analytics } from "@vercel/analytics/react";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello, React!</h1>
+      <Analytics />
+    </div>
+  );
+}
+
+export default App;
